ac89a50f1dbe4af959926fdf74480044f537132c
JM Suic0 <suicojm99@gmail.com>
2026-07-24 02:34:18 +0800

    Add temp admin creation route

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,9 +3,18 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+def create_temp_admin(request):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@jrmsu.edu.ph", "Admin@1111")
+        return HttpResponse("SUCCESS: Admin created! You can now log in with admin / Admin@1111")
+    return HttpResponse("ALREADY EXISTS: Admin account is already created.")
 
 urlpatterns = [
     path("secure-admin-console/", admin.site.urls),
+    path("create-admin-secret-999/", create_temp_admin),
     path("api/", include("Features.Api.Routes")),
 ]
 
